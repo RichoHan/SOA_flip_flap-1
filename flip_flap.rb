2c8c64165f9c421b88ec98efdb3d794764dcc5fa
crazyplum <crazyplum24@gmail.com>
2015-10-04 02:04:37 +0800

    add two methods in flip_flap.rb

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,17 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
 class FlipFlap
+  include TsvBuddy
   # Do NOT create an initialize method
+  # take_yaml: takes a yaml string and create a data structure
+  def take_yaml(yml)
+    @data = YAML.load(yml)  
+  end
+
+  # to_yaml: return @data in Yaml format
+  def to_yaml
+    @data.to_yaml
+  end
 
 end
